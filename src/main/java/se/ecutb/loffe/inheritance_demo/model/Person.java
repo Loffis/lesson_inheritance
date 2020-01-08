1e8e7ac2473bba0e96dd1760961502339224cf4a
Loffe Knutsson <loffe.knutsson@gmail.com>
2020-01-08 13:25:02 +0100

    first commit

---
+++ src/main/java/se/ecutb/loffe/inheritance_demo/model/Person.java
@@ -0,0 +1,39 @@
+package se.ecutb.loffe.inheritance_demo.model;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    protected String name;
+    protected LocalDate birthDate;
+
+    public Person(String name, LocalDate birthDate){
+        // super(); // pekar på den constructor som är inbyggd i object. Osynlig egentligen. Sker automatiskt.
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
